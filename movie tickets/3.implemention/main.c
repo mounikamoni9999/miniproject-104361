c9157f66ee1e8bca329423338de34ad79865c6b0
mounikamoni9999 <71550057+mounikamoni9999@users.noreply.github.com>
2020-11-10 22:50:41 +0530

    Create main.c

---
+++ movie tickets/3.implemention/main.c
@@ -0,0 +1,11 @@
+#include "inc\omt.h"
+#include "test\test_omt.h"
+
+int main()
+{
+    printf("\n****************************************online movie tickets booking*****************************************\n");
+
+    test_main();
+
+    return 0;
+}
