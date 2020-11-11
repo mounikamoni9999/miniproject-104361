90bb50fbe01b08237308f936d8b862060cabf9b9
mounikamoni9999 <71550057+mounikamoni9999@users.noreply.github.com>
2020-11-11 21:54:20 +0530

    Create OMT.h

---
+++ movie tickets/3.implemention/inc/OMT.h
@@ -0,0 +1,52 @@
+#ifndef __OMT_H__
+#define __OMT_H__
+
+#include<stdio.h>
+#include<iostream.h>
+//#include<curses.h>
+#include<string.h>
+#include<stdlib.h>
+
+void returnfunc(void);
+void returnfunc1(void);
+
+void mainmenu(void);
+
+int checkid(int);
+
+void administration(void);
+void customer(void);
+
+void insert_details(void);
+void viewAll(void);
+void find(void);
+void book_ticket(void);
+void old_record(void);
+
+void movie code (void);
+void name (void);
+void release date(void);
+void ticket price(void);
+
+void Record insert Successful(void);
+int getdata();
+
+void view all(void);
+int getdata();
+
+void find(void);
+void movie code(void);
+void record found(void);
+int getdata();
+void movie name (void);
+void movie date(void);
+void price of ticket(void);
+
+void book ticket(void);
+void name(void);
+void mobile number(void);
+void number of tickets(void);
+
+void old record(void);
+int add(int,int);
+#endif 
